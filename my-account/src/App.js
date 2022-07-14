47cbbcfe4e9873bd6b789677cd3e4bf0140c5bd4
Parsatag462 <mr.nitsuji462@gmail.com>
2022-07-14 17:46:40 +0000

    account

---
+++ my-account/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AccountPage from './Compenents/AccountPage';
+
+function App() {
+  return (
+    <div className="App">
+      <AccountPage/>
+    </div>
+  );
+}
+
+export default App;
